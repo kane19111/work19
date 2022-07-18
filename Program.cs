67854490b66086f6bd894c33dc733fbf0491929f
«Sergey» <kane1911@yandex>
2022-07-18 09:50:31 +0300

    Решил задачу №19

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+        bool palindrome = true;
+        int x = int.Parse(Console.ReadLine());
+        int c = x.ToString().Length - 1;
+        string b = x.ToString();
+        for (int i = 0; i < c; i++)
+            if (b[i] != b[c - i])
+                palindrome = false;
+        if (palindrome == true)
+            Console.Write("Число является палиндромом.");
+        else Console.Write("Число не является палиндромом. ");
+        Console.ReadKey();
